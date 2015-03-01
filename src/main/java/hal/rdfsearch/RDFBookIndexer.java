34a7789323555c62d3a953af2248b09636c9c6a7
Stéphane Lopes <stephane.lopes@prism.uvsq.fr>
2015-03-01 16:05:01 +0100

    feat(index): indexe les livres du fichier de données RDF

--- src/main/java/hal/rdfsearch/RDFBookIndexer.java
+++ src/main/java/hal/rdfsearch/RDFBookIndexer.java
@@ -6,10 +6,22 @@ import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.util.FileManager;
 import com.hp.hpl.jena.vocabulary.DCTerms;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Paths;
+import java.util.Date;
 
 import static hal.rdfsearch.RDFSearch.ENVIRONMENT;
 
@@ -22,6 +34,9 @@ import static hal.rdfsearch.RDFSearch.ENVIRONMENT;
 public class RDFBookIndexer {
     /** Journal. */
     public static final Logger logger = LoggerFactory.getLogger(RDFBookIndexer.class);
+
+    /** Répertoire où sera stocké l'index. */
+    public static final String indexPath = "index";
 
     /** Modèle Jena pour les livres. */
     private Model bookModel;
@@ -36,12 +51,44 @@ public class RDFBookIndexer {
         bookModel.read(in, "");
     }
 
-    public void indexBooks() {
-        logger.info("Indexation des livres, i.e. ressources avec un titre");
+    public void indexBooks() throws IOException {
+        logger.info("Indexation des livres, i.e. ressources avec un titre dans le répertoire {}", indexPath);
+        Date start = new Date();
+
+        Directory dir = FSDirectory.open(Paths.get(indexPath));
+        Analyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+        iwc.setOpenMode(IndexWriterConfig.OpenMode.CREATE); // remplace l'index s'il existe
+        IndexWriter writer = new IndexWriter(dir, iwc);
+
         ResIterator iter = bookModel.listResourcesWithProperty(DCTerms.title);
         while (iter.hasNext()) {
             Resource r = iter.nextResource();
-            logger.debug("indexation du livre {}", r);
+            indexBook(r, writer);
         }
+
+        writer.close();
+
+        Date end = new Date();
+        logger.info("Indexation en {} millisecondes", end.getTime() - start.getTime());
+    }
+
+    /**
+     * Ajoute un livre dans l'index.
+     *
+     * @param livre le livre à ajouter
+     * @param writer l'index
+     */
+    private void indexBook(Resource livre, IndexWriter writer) throws IOException {
+        Document doc = new Document();
+
+        Field uriField = new StringField("uri", livre.getURI(), Field.Store.YES);
+        doc.add(uriField);
+
+        String title = livre.getProperty(DCTerms.title).getString();
+        Field titleField = new StringField("title", title, Field.Store.NO);
+
+        logger.debug("indexation du livre \"{}\" ({})", title, livre);
+        writer.addDocument(doc);
     }
 }
