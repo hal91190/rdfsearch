988c9f69a158a5e212663956870c4f54e6b40236
Stéphane Lopes <stephane.lopes@uvsq.fr>
2023-01-20 10:52:45 +0100

    feat(build): met à jour et migre le build vers le DSL Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    application
+}
+
+application {
+    mainClass.set("hal.rdfsearch.RDFSearch")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+val jenaVersion = "3.1.1"
+val luceneVersion = "6.4.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:1.7.10")
+    implementation("ch.qos.logback:logback-classic:1.1.2")
+    implementation("ch.qos.logback:logback-core:1.1.2")
+
+    testImplementation("junit:junit:4.12")
+
+    implementation("org.apache.jena:apache-jena-libs:$jenaVersion") {
+        exclude(module = "slf4j-log4j12")
+        exclude(module = "log4j")
+    }
+
+    implementation("org.apache.lucene:lucene-core:$luceneVersion")
+    implementation("org.apache.lucene:lucene-analyzers-common:$luceneVersion")
+    implementation("org.apache.lucene:lucene-queryparser:$luceneVersion")
+}
